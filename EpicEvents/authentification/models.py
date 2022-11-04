2a7458e01e4b72ae2e6d0e338cc4d91ddc6a023f
ydjabela <ydjabela@blendbow.com>
2022-11-04 17:50:52 +0100

    creat  users  models

--- EpicEvents/authentification/models.py
+++ EpicEvents/authentification/models.py
@@ -1,5 +1,71 @@
+from __future__ import unicode_literals
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
+
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+
+    SALES = 'sales_member'
+    MANAGEMENT = 'management_member'
+    SUPPORT = 'support_member'
+    ROLE_CHOICES = (
+        (SALES, 'sales_member'),
+        (MANAGEMENT, 'management_member'),
+        (SUPPORT, 'support_member')
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+
+    username = None
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    objects = UserManager()
+
+    def __str__(self):
+        return f'{self.email}'
 
 
 class Client(models.Model):
