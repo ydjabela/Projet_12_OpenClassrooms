9457723b9c47c24da0a1ae46ca5bb3d8d7320583
ydjabela <ydjabela@blendbow.com>
2022-11-12 23:41:50 +0100

    update urls

--- EpicEvents/EpicEvents/urls.py
+++ EpicEvents/EpicEvents/urls.py
@@ -15,11 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from authentification.views import ClientView
-router = DefaultRouter()
-router.register(r'', ClientView, basename='client')
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'client/', include(router.urls))
+    path('client/', include("authentification.urls")),
+    path('client/<int:client_id>/contrat/', include("contrat.urls"))
 ]
