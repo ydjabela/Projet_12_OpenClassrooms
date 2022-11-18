cf6b77f7373d4f57f0a353a6e24d92fbf72c3068
ydjabela <ydjabela@blendbow.com>
2022-11-18 15:27:08 +0100

    delete evenement

--- EpicEvents/événement/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import SimpleRouter
-from .views import EventView
-
-
-router = SimpleRouter()
-router.register(r'', EventView, basename='event')
-
-urlpatterns = router.urls
